297f598bb3f2baa1889a29695bf504bdfb921e65
LiamFengLin <linfeng@berkeley.edu>
2016-04-02 01:35:31 +0000

    added a few more tests

--- spec/admin/users_controller_spec.rb
+++ spec/admin/users_controller_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
+require 'rails_helper'
+include Devise::TestHelpers
 
-describe describe Admin::UsersController do
+describe Admin::UsersController, type: :controller do
+  render_views
+  
+  let(:admin_user) { AdminUser.create!(email: 'admin12@example.com', password: 'password')}
+
+  before(:each) do
+    sign_in admin_user
+  end
+  
+  describe "GET index" do
+    it "displays unapproved users" do
+      a_user = User.create({email: "test@berkeley.edu", name: "tom lin", phone: "5105415041", address: "2715 Dwight Way, Berkeley CA", password: "password"}) 
+      get :index
+      expect(assigns(:users)).to eq([a_user])
+    end
+  end
+  
+  describe "GET index" do
+    it "displays approved users" do
+      a_user = User.create({email: "test@berkeley.edu", name: "tom lin", phone: "5105415041", address: "2715 Dwight Way, Berkeley CA", password: "password", approved: true}) 
+      get :index
+      expect(assigns(:users)).to eq([])
+    end
+  end
 end
