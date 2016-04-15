041860c34ece6f3002e54aaa232c874010c47440
Eric Nguyen <neric@berkeley.edu>
2016-04-15 19:54:34 +0000

    Finished backend for uploading answer images

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,11 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   validates :content, presence: true
   mount_uploader :answer_image, AnswerImageUploader
+  
+  def image_url
+    answer_image.url
+  end
+  
+  alias_method :img_url, :image_url
+  alias_method :image, :image_url
 end
