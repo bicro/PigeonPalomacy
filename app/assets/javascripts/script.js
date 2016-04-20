68659b08628514c5c7999e8028630e28fadf4c6c
VictoriaSnow <victoriasnow1124@gmail.com>
2016-04-20 02:30:19 -0700

    css fixes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-	console.log('clicked');
+	$('.survey').addClass('survey-active');
+	$('.nav').addClass('nav-active');
 	$('.nav-link-login').click(function() {
 		console.log('clicked');
 		if ($('.nav-link-item-dropdown').hasClass('dropdown-active')) {
-			$('.nav-link-item-dropdown').hide('fade', 300);
+			$('.nav-link-item-dropdown').hide();
 			$('.nav-link-item-dropdown').removeClass('dropdown-active')
 		} else {
 			$('.nav-link-item-dropdown').addClass('dropdown-active');
-			$('.nav-link-item-dropdown').show('fade', 500);
+			$('.nav-link-item-dropdown').show();
 		};
 	});
 });
