c99a8426732fe4d4b3a7cd4e23a0e44ad587963f
armaniferrante <armaniferrante@gmail.com>
2016-04-22 14:00:39 -0700

    readded begn rescue since travis failed

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -16,24 +16,32 @@ class Submission < ActiveRecord::Base
     # Returns a list of the closest experts to my
     # submission. Assumes this object is geocoded.
     def experts
-        number_experts = 3
+        begin
+            number_experts = 3
 
-        experts = []
+            experts = []
 
-        User.all.each do |pigeon_expert| 
-            distance = distance_to([pigeon_expert.latitude, 
-                                    pigeon_expert.longitude])    
+            User.all.each do |pigeon_expert| 
+                distance = distance_to([pigeon_expert.latitude, 
+                                        pigeon_expert.longitude])    
 
-            index_and_distance = largest_distance(experts)
+                index_and_distance = largest_distance(experts)
 
-            if experts.size < number_experts
-                experts << pigeon_expert
-            elsif experts.size == number_experts && distance < index_and_distance[1]
-                experts.delete_at(index_and_distance[0])
-                experts << pigeon_expert
+                if experts.size < number_experts
+                    experts << pigeon_expert
+                elsif experts.size == number_experts && distance < index_and_distance[1]
+                    experts.delete_at(index_and_distance[0])
+                    experts << pigeon_expert
+                end
+            end
+            return experts.sort_by{ |expert| self.distance_to([expert.latitude, expert.longitude])}
+        rescue
+            if User.all.count < 3
+                return User.all
+            else
+                return User.take(3)
             end
         end
-        return experts.sort_by{ |expert| self.distance_to([expert.latitude, expert.longitude])}
     end
 
     # Returns [index, distance], where index is the position
