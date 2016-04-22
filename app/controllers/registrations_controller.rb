13f279807ddb9723564dc6f2f375038157506e4f
LiamFengLin <linfeng@berkeley.edu>
2016-04-22 13:43:27 -0700

    fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,29 +1,2 @@
 class RegistrationsController < Devise::RegistrationsController
-
-  # private
-
-  # def sign_up_params
-
-  #   params.require(:user).permit(
-  #     :name, 
-  #     :email, 
-  #     :phone, 
-  #     :password, 
-  #     :password_confirmation, 
-  #     :street_address_1, 
-  #     :street_address_2, 
-  #     :city, 
-  #     :state, 
-  #     :zipcode, 
-  #     :country, 
-  #     :preferred_contact, 
-  #     :expertise_description, 
-  #     :shelter_image_1,
-  #     :shelter_image_2
-  #   )
-  # end
-
-  # def account_update_params
-  #   params.require(:user).permit()
-  # end
 end
